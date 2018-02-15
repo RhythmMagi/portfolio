19db553c8b279b1de09dd81e2a61e191933bd3e6
Jeffrey Schrock <rhythmmagi@gmail.com>
2018-02-15 01:50:24 +0000

    Add about, stylized jumbotron, add fonts, add project images and descriptions

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var express = require("express"),
-    app = express();
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser");
     
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("index");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
 });
     
 app.listen(process.env.PORT, process.env.IP, function(){
