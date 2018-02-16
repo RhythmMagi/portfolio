a194c24d3cbbb6a24f3827c5982be48bab4ab0b6
Jeffrey Schrock <rhythmmagi@gmail.com>
2018-02-16 02:37:34 +0000

    Fix margins, update footer, add icons to languages, remove navbar

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("index");
 });
-
-app.get("/about", function(req, res){
-    res.render("about");
-});
     
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Portfolio has started");
